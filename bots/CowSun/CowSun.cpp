8c1b33a7847f5a66c71add30fc16e0b4f6476a66
jbrakensiek <jbrakensiek@noreply.github.com>
2014-05-27 21:41:51 -0400

    Can now implement cow/farmer in same bot.

--- bots/CowSun/CowSun.cpp
+++ bots/CowSun/CowSun.cpp
@@ -2,13 +2,15 @@
 
 #include "mm-client.h"
 
+using namespace std;
+
 // This is the name that the driver will refer to your bot as.
 const char* BOT_NAME = "CowSun";
 
 // Return whether setup was successful, bot dies if 0.
 int client_setup(int *argc, char ***argv)
 {
-	return 1;
+  return 1;
 }
 
 void game_setup_cow (const struct player_data farmer) {
@@ -30,10 +32,31 @@ void game_setup(const struct player_data* players)
   }
 }
 
+void turn_start_cow(unsigned int roundnum, const struct player_data farmer) {
+
+}
+
+void turn_start_farmer (unsigned int roundnum, const struct player_data cow) {
+
+}
+
 // When this function is called, your move is about to start. You can make decisions here if you would like.
 void turn_start(unsigned int roundnum, const struct player_data* players)
 {
+  if (SELF.id == 0) { // cow
+    turn_start_cow (roundnum, players[1]);
+  }
+  else { //farmer
+    turn_end_cow (roundnum, cow[1]);
+  }
+}
 
+void player_turn_cow (struct player_unit* unit, const struct player_data farmer) {
+
+}
+
+void player_turn_farmer(struct player_unit* unit, const struct player_data cow) {
+  
 }
 
 // A unit (id + coordinates) is provided. Update its row/col before exiting.
@@ -44,6 +67,12 @@ void player_turn(struct player_unit* unit, const struct player_data* players)
 
 	// unit->row
 	// unit->col
+  if (SELF.id == 0) { // cow
+    player_turn_cow (unit, players[1]);
+  }
+  else { // farmer
+    player_turn_farmer (unit, players[0]);
+  }
 }
 
 // This function is called at the end of the game, as a courtesy.
